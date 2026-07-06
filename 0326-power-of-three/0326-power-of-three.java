5be1f79d8bdf7db5ed46f30104806cf073d9fa4e
swami <chereddyswami025@gmail.com>
2026-07-06 18:58:45 +0530

    Time: 8 ms (91.63%), Space: 45.8 MB (92.73%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -4,14 +4,14 @@ class Solution {
         if(n%2==0){
             return false;
         }
-        int m=n;
-        int sum=0;
-        while(m>0){
-            int rem=m%10;
-            sum+=rem;
-            m/=10;
-        }
-        if(sum%3!=0&&n!=1){
+        // int m=n;
+        // int sum=0;
+        // while(m>0){
+        //     int rem=m%10;
+        //     sum+=rem;
+        //     m/=10;
+        // }
+        if(n%3!=0&&n!=1){
             return false;
 
         }
