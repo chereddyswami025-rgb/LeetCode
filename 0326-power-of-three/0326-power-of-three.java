df3917abb62bd728fca708611e70f31c4cb661ac
swami <chereddyswami025@gmail.com>
2026-07-06 19:00:02 +0530

    Clean up commented code in power-of-three.java
    Removed commented-out code for clarity.

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -4,13 +4,7 @@ class Solution {
         if(n%2==0){
             return false;
         }
-        // int m=n;
-        // int sum=0;
-        // while(m>0){
-        //     int rem=m%10;
-        //     sum+=rem;
-        //     m/=10;
-        // }
+  
         if(n%3!=0&&n!=1){
             return false;
 
