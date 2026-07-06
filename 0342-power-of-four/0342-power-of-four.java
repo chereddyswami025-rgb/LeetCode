12ac67a7cab78dbd86657d89f610f293f2b4ff56
swami <chereddyswami025@gmail.com>
2026-07-06 18:51:30 +0530

    Time: 1 ms (89.56%), Space: 42.4 MB (69.64%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -9,28 +9,8 @@ class Solution {
             return false;
         }
 
-        // int m = n;
-        // int rev = 0;
-
-        // int cnt = 0;
-        // while (m != 0) {
-        //     int rem = m % 10;
-        //     rev = rev * 10 + rem;
-        //     m /= 10;
-        //     if (cnt == 2) {
-        //         break;
-        //     }
-        // }
-
-        // int l = 0;
-        // while (rev != 0) {
-        //     int r = rev % 10;
-        //     l = l * 10 + r;
-        //     rev /= 10;
-           
-             
-
-        // }
+       
+     
 
         if (n % 4 != 0) {
             return false;
