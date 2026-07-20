64840c9cc6c3648f97d334beaca76a577375030d
swami <chereddyswami025@gmail.com>
2026-07-20 11:56:03 +0530

    Time: 0 ms (100%), Space: 48.4 MB (50.32%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -7,18 +7,14 @@ class Solution {
             if(nums[mid]==target){
                 return mid;
             }
-
            else if(target>nums[mid]){
                 left=mid+1;
             }
-
             else{
                right=mid-1;
             }
 
-
         }
-
         return -1;
     }
 }
